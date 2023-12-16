c5645ee289601b815302c7396a219bdb20e0798e
CamilleusRex <camilleusrex@gmail.com>
2023-12-16 14:55:23 +0100

    added basic students data base

---
+++ data.sql
@@ -0,0 +1,12 @@
+/*students*/
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    group_id INTEGER
+);
+
+INSERT INTO students (name, group_id) VALUES
+('John Doe', 1),
+('Jane Smith', 2),
+('Bob Johnson', 1),
+;
