a2653a7f0cbc1deeee5f4941cba0895c21bdb244
CamilleusRex <camilleusrex@gmail.com>
2023-12-16 14:58:22 +0100

    added more students

--- data.sql
+++ data.sql
@@ -9,4 +9,7 @@ INSERT INTO students (name, group_id) VALUES
 ('John Doe', 1),
 ('Jane Smith', 2),
 ('Bob Johnson', 1),
+('Camilleus Rex', 1),
+('Octavian August', 1),
+('Matthias El Guerriero', 1),
 ;
