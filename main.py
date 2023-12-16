d6cdd4fa40f9fca2055650b025956d16e0ecfdac
CamilleusRex <camilleusrex@gmail.com>
2023-12-16 14:42:58 +0100

    tables are done

--- main.py
+++ main.py
@@ -16,3 +16,35 @@ cursor.execute('''
         group_id INTEGER
     )
 ''')
+
+cursor.execute('''
+    CREATE TABLE groups (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE professors (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE subjects (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        professor_id INTEGER
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE grades (
+        id INTEGER PRIMARY KEY,
+        student_id INTEGER,
+        subject_id INTEGER,
+        grade INTEGER,
+        date TEXT
+    )
+''')
