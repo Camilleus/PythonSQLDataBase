4520bf9b0ebb71a82ad80e9fe366c51966a9f932
CamilleusRex <camilleusrex@gmail.com>
2023-12-16 14:31:35 +0100

    added table creation

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ fake = Faker()
 
 conn = sqlite3.connect('university.db')
 cursor = conn.cursor()
+
+
+
+cursor.execute('''
+    CREATE TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        group_id INTEGER
+    )
+''')
