209b7c06a051dd134f865dee81497120af10591d
CamilleusRex <camilleusrex@gmail.com>
2023-12-27 15:50:57 +0100

    added students

--- seed.py
+++ seed.py
@@ -1,5 +1,19 @@
-from faker import Faker
 import random
+from faker import Faker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from models import Session, Student, Group, Professor, Subject, Grade
 
 fake = Faker()
+
+def create_students(session, num_students):
+    students = []
+    for _ in range(num_students):
+        student = Student(
+            name=fake.name(),
+            email=fake.email(),
+            group_id=random.choice(session.query(Group.id).all())[0]
+        )
+        students.append(student)
+        session.add(student)
+    session.commit()
